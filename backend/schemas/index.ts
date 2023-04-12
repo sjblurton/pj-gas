e5b74ac7f53311f4f06120eb2c41e328e5865a42
sjlurton <sblurton@hotmail.co.uk>
2023-04-12 11:49:29 +0100

    removed the CMS

--- backend/schemas/index.ts
+++
@@ -1,7 +0,0 @@
-import company from './settings/company'
-import about from './pages/about'
-import home from './pages/home'
-import keyPoints from './items/keyPoints'
-import contact from './pages/contact'
-
-export const schemaTypes = [company, about, home, keyPoints, contact]
